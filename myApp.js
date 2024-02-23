17e3301bafbcb029fa4d789d4eb81a56446b895f
DarioFunes89 <dariofunes89@gmail.com>
2024-02-23 10:02:17 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -4,10 +4,22 @@ let app = express();
 
 console.log("Hello World");
 
+app.use(function middleware(req, res, next) {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+  });
+
 app.use("/public",express.static(__dirname + "/public"))
 
 app.get('/', (req, res) =>{
    res.sendFile(__dirname + '/views/index.html')
+});
+
+app.get("/now", (req,res,next)=>{
+    req.time = new Date().toString()
+    next()
+},(req,res)=> {
+    res.json({"time": req.time})
 });
 
 app.get('/json',(req,res)=>{
